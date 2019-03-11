5cfbd2af1ab7f8bea2ce6151e36a77f55bfeb676
y <n>
2019-03-11 14:55:14 -0400

    new components and styles added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import classes from './Layout.css';
+
 const layout = (props) => (
-  <>
+  <React.Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
-  </>
+  </React.Fragment>
 );
 
 export default layout;
